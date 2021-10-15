335ab46a71fa225b4bfffb6b4c02cf8a2cd62221
Adrian Garcia Cubas <agc819@alumnos.unican.es>
2021-10-15 14:55:06 +0200

    Empezada la implementacion de la ventana flotante de filtrar (solo muestra el titulo)

--- AndroidProject/app/src/main/java/com/isunican/eventossantander/view/events/EventsActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/eventossantander/view/events/EventsActivity.java
@@ -1,6 +1,7 @@
 package com.isunican.eventossantander.view.events;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,17 +20,24 @@ import com.isunican.eventossantander.presenter.events.EventsPresenter;
 import com.isunican.eventossantander.view.eventsdetail.EventsDetailActivity;
 import com.isunican.eventossantander.view.info.InfoActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class EventsActivity extends AppCompatActivity implements IEventsContract.View {
+public class EventsActivity extends AppCompatActivity implements IEventsContract.View, View.OnClickListener {
 
     private IEventsContract.Presenter presenter;
     private EventArrayAdapter adapter;
+
+    private Button btnFiltrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //enlazamos con el layout y asignamos listener para el boton de filtrar
+        btnFiltrar = findViewById(R.id.btn_filtrar);
+        btnFiltrar.setOnClickListener(this);
 
         presenter = new EventsPresenter(this);
     }
@@ -96,4 +106,30 @@ public class EventsActivity extends AppCompatActivity implements IEventsContract
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.btn_filtrar){
+            AlertDialog ad = onFilterAlertDialog();
+            ad.show();
+        }
+    }
+
+    @Override
+    public AlertDialog onFilterAlertDialog(){
+        //Creamos una lista donde meter los eventos que cumplan el filtro
+        List eventosFiltrados = new ArrayList<Event>();
+
+        //Creamos una AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //Le ponemos un titulo
+        builder.setTitle("Filtrar");
+
+        //Creamos los elementos de la seleccion de tipo multiple
+        //builder.setMultiChoiceItems();
+
+        return builder.create();
+    }
+
 }
