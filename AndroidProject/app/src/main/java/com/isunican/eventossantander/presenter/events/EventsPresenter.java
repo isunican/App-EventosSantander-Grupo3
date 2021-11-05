fdf21830720a22ef27a38a046b9a0200862aa492
lopalon <alvarolopalo4@gmail.com>
2021-11-05 13:08:20 +0100

    Arreglados code smells

--- AndroidProject/app/src/main/java/com/isunican/eventossantander/presenter/events/EventsPresenter.java
+++ AndroidProject/app/src/main/java/com/isunican/eventossantander/presenter/events/EventsPresenter.java
@@ -32,7 +32,6 @@ public class EventsPresenter implements IEventsContract.Presenter {
     public EventsPresenter(IEventsContract.View view) {
         this.view = view;
         loadData();
-
     }
 
     private void loadData() {
