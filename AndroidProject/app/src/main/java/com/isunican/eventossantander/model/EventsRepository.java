c932a8bbd4265a971866e0990ff78cac961ed7dd
Adrian Garcia Cubas <agc819@alumnos.unican.es>
2021-11-22 14:43:59 +0100

    Finalizada historia de usuario OrdenarPorHoraDeComienzo

--- AndroidProject/app/src/main/java/com/isunican/eventossantander/model/EventsRepository.java
+++ AndroidProject/app/src/main/java/com/isunican/eventossantander/model/EventsRepository.java
@@ -18,7 +18,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
 public class EventsRepository {
     private EventsRepository() {
         throw new IllegalStateException("Utility class");
